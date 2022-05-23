f52de71fe73708089645ee585820619802856178
Shannon Spragg <shannonspragg@u.boisestate.edu>
2022-05-23 10:38:41 -0600

    updating

--- analysis code scripts/8. Kernel Density Bear Reports.R
+++ analysis code scripts/8. Kernel Density Bear Reports.R
@@ -18,13 +18,13 @@ library(spatialEco)
 library(terra)
 
 # Bring in Data: ----------------------------------------------------------
-warp.crop.10km <- st_read("/Users/shannonspragg/SOI-Grizz/Data/processed/warp_crop_10km_buf.shp")
-south.int.10k.buf <- st_read("/Users/shannonspragg/SOI-Grizz/Data/processed/SOI_10km_buf.shp")
+warp.crop.10km <- st_read("Data/processed/warp_crop_10km_buf.shp")
+south.int.10k.buf <- st_read("Data/processed/SOI_10km_buf.shp")
 
 # Filter Our Data: --------------------------------------------------------
 
-# Create a Black bears and Grizzly Population raster:
-# Crop our df to black bears and grizz seperately
+  # Create a Black bears and Grizzly Population raster:
+  # Crop our df to black bears and grizz seperately
 black.bears <- warp.crop.10km %>% 
   dplyr::filter(warp.crop.10km$spcs_nm == "BLACK BEAR" )
 
@@ -38,19 +38,19 @@ b.bears.rast <- raster(black.bears)
 g.bears.rast <- raster(grizz.bears)
 
 # Format all Data: --------------------------------------------------------
-# First, our boundary shapefile:
+  # First, our boundary shapefile:
 s <- south.int.10k.buf
 w <- as.owin(s)
 w.km <- rescale(w, 1000)
 
-# Next, our black bears points shapefile:
+  # Next, our black bears points shapefile:
 sw <- black.bears
 bb.pts <- as.ppp(sw)
 marks(bb.pts) <- NULL
 bb.pts <- rescale(bb.pts, 1000)
 Window(bb.pts) <- w
 
-# Lastly, our grizzly bears points shapefile:
+  # Lastly, our grizzly bears points shapefile:
 gw <- grizz.bears
 gb.pts <- as.ppp(gw)
 marks(gb.pts) <- NULL
@@ -58,7 +58,7 @@ gb.pts <- rescale(gb.pts, 1000)
 Window(gb.pts) <- w
 
 # Visualize our Data Points -----------------------------------------------
-# Plot the points and boundary:
+  # Plot the points and boundary:
 plot(bb.pts, main=NULL, cols=rgb(0,0,0,.2), pch=20)
 
 
@@ -67,31 +67,31 @@ plot(bb.pts, main=NULL, cols=rgb(0,0,0,.2), pch=20)
 # BLACK BEARS:----------
 Q.b <- quadratcount(bb.pts, nx= 6, ny=3) # assign quadrats
 
-# Plot our points with quadrats:
+  # Plot our points with quadrats:
 plot(bb.pts, pch=20, cols="grey70", main=NULL)  # Plot points
 plot(Q.b, add=TRUE)  # Add quadrat grid
 
-# Compute the density for each quadrat
+  # Compute the density for each quadrat
 Q.bd <- intensity(Q.b)
 
-# Plot the density
+  # Plot the density
 plot(intensity(Q.b, image=TRUE), main=NULL, las=1)  # Plot density raster
 plot(bb.pts, pch=20, cex=0.6, col=rgb(0,0,0,.5), add=TRUE) # add points
 # The density values are reported as the number of points (stores) per square kilometer, per quadrat
 
-# Kernel Density Estimate:
+  # Kernel Density Estimate:
 K1.bb <- raster::density(bb.pts) # Using the default bandwidth
 plot(K1.bb, main=NULL, las=1)
 contour(K1.bb, add=TRUE)
 title("Black Bear KDE for Southern Interior") 
 
-# Do this with a 50km bandwidth:
+  # Do this with a 50km bandwidth:
 K2.bb <- density(bb.pts, sigma=50) # Using a 50km bandwidth
 plot(K2.bb, main=NULL, las=1)
 contour(K2.bb, add=TRUE)
 title("Black Bear KDE w/ 50km Bandwith for Southern Interior") 
 
-# Try out with different function:
+  # Try out with different function:
 K3.bb <- density(bb.pts, kernel = "disc", sigma=50) # Using a 50km bandwidth
 plot(K3.bb, main=NULL, las=1)
 contour(K3.bb, add=TRUE)
@@ -100,31 +100,31 @@ title("Black Bear KDE w/ 50km Bandwidth for Southern Interior")
 # GRIZZLY BEARS: ---------
 Q.g <- quadratcount(gb.pts, nx= 6, ny=3) # assign quadrats
 
-# Plot our points with quadrats:
+  # Plot our points with quadrats:
 plot(gb.pts, pch=20, cols="grey70", main=NULL)  # Plot points
 plot(Q.g, add=TRUE)  # Add quadrat grid
 
-# Compute the density for each quadrat
+  # Compute the density for each quadrat
 Q.gd <- intensity(Q.g)
 
-# Plot the density
+  # Plot the density
 plot(intensity(Q.g, image=TRUE), main=NULL, las=1)  # Plot density raster
 plot(gb.pts, pch=20, cex=0.6, col=rgb(0,0,0,.5), add=TRUE)  # Add points
 # The density values are reported as the number of points (stores) per square kilometer, per quadrat
 
-# Kernel Density Estimate:
+  # Kernel Density Estimate:
 K1.gb <- density(gb.pts) # Using the default bandwidth
 plot(K1.gb, main=NULL, las=1)
 contour(K1.gb, add=TRUE)
 title("Grizzly Bear KDE for Southern Interior") # Add points
 
-# Do this with a 50km bandwidth:
+  # Do this with a 50km bandwidth:
 K2.gb <- density(gb.pts, sigma=50) # Using a 50km bandwidth
 plot(K2.gb, main=NULL, las=1)
 contour(K2.gb, add=TRUE)
 title("Grizzly Bear KDE w/ 50km Bandwidth for Southern Interior") 
 
-# Try out with different function:
+  # Try out with different function:
 K3.gb <- density(gb.pts, kernel = "disc", sigma=50) # Using a 50km bandwidth
 plot(K3.gb, main=NULL, las=1)
 contour(K3.gb, add=TRUE)
@@ -150,7 +150,7 @@ title("Grizzly & Black Bear KDE Correlation for Southern Interior")
 bears.kde.50km.corr <- rasterCorrelation(K2.bb.raster, K2.gb.raster, type = "pearson")
 plot(bears.kde.50km.corr)
 
-# Filter these for values less than 0.7:
+  # Filter these for values less than 0.7:
 bears.kde.corr[bears.kde.corr > 0.7] <- 1
 bears.kde.corr[bears.kde.corr < 0.69] <- 0
 
@@ -164,22 +164,22 @@ title("Grizzly & Black Bear KDE Correlation for Southern Interior")
 crs(bears.kde.corr) <- CRS('+init=EPSG:3153')
 
 
-# Reproject the vector data:
+  # Reproject the vector data:
 b.bears.reproj <- st_make_valid(black.bears) %>% 
   st_transform(crs=crs(bears.kde.corr))
 g.bears.reproj <- st_make_valid(grizz.bears) %>% 
   st_transform(crs=crs(bears.kde.corr))
 
-# Make these spatvectors & rasters:
+  # Make these spatvectors & rasters:
 g.bears.vect <- vect(g.bears.reproj)
 b.bears.vect <- vect(b.bears.reproj)
 
 bears.kde.corr.sr <- as(bears.kde.corr, "SpatRaster")
 
-#match the extents
+  # Match the extents
 ext(bears.kde.corr.sr) <- ext(b.bears.vect) 
 
-#plot all together:
+  # Plot all together:
 plot(bears.kde.corr.sr)
 plot(b.bears.vect, pch=2, col = "red", add=TRUE) #this works...
 plot(g.bears.vect, pch=19, col = "black",add=TRUE)
@@ -192,10 +192,10 @@ legend("topright",   # set position
 
 
 # Write as .tif files: ----------------------------------------------------
-raster::writeRaster(K1.bb.raster, "/Users/shannonspragg/SOI-Grizz/Data/processed/black_bear_kde.tif")
-raster::writeRaster(K2.bb.rastr, "/Users/shannonspragg/SOI-Grizz/Data/processed/black_bear_kde_50km.tif")
-raster::writeRaster(K1.gb.raster, "/Users/shannonspragg/SOI-Grizz/Data/processed/grizz_bear_kde.tif")
-raster::writeRaster(K2.gb.raster, "/Users/shannonspragg/SOI-Grizz/Data/processed/grizz_bear_kde_50km.tif")
+raster::writeRaster(K1.bb.raster, "Data/processed/black_bear_kde.tif")
+raster::writeRaster(K2.bb.rastr, "Data/processed/black_bear_kde_50km.tif")
+raster::writeRaster(K1.gb.raster, "Data/processed/grizz_bear_kde.tif")
+raster::writeRaster(K2.gb.raster, "Data/processed/grizz_bear_kde_50km.tif")
 
-raster::writeRaster(bears.kde.corr, "/Users/shannonspragg/SOI-Grizz/Data/processed/gbears_kde_correlation.tif")
+raster::writeRaster(bears.kde.corr, "Data/processed/gbears_kde_correlation.tif")
 
